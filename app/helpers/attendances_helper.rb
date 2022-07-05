760b5e0c3d33e49d771041ab11de5f348b7a3911
OrikoTanaka <oriko644@gmail.com>
2022-07-05 07:37:46 +0000

    Finish add-user-attendances 7/5

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -6,7 +6,12 @@ module AttendancesHelper
       return '出勤' if attendance.started_at.nil?
       return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
-    
-    false
+    #　どれにも当てはまらなかった場合はfalseを返します。
+    return false
+  end
+  
+  # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
   end
 end
